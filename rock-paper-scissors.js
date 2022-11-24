50bad37fef315c720fda5ef32967dac64159ff95
Antony <antony.caron.burdiak@gmail.com>
2022-11-24 13:16:35 +0100

    Add personnal puzzle 'rock-paper-scissors'

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,6 @@
-let scorePlyr1 = 0
-let scorePlyr2 = 0
+const rounds = 3
+let p1score = 0
+let p2score = 0
 
 function choose() {
   let choice = Math.floor((Math.random() * 3) + 1)
@@ -10,57 +11,39 @@ function choose() {
   }
 }
 
+function versus() {
+  let p1choice = choose()
+  let p2choice = choose()
+  console.log('P1',p1choice)
+  console.log('P2',p2choice)
+  if (p1choice === p2choice) {
+    console.log('PAR !')
+  } else {
+    if (p1choice === 'rock') {
+      if (p2choice === 'paper') p2score++
+      else p1score++
+    } else if (p1choice === 'paper') {
+      if (p2choice === 'scissors') p2score++
+      else p1score++
+    } else if (p1choice === 'scissors') {
+      if (p2choice === 'rock') p2score++
+      else p1score++
+    }
+    console.log('P1',p1score,'- P2',p2score)
+  }
+  console.log('')
+}
+
 function game() {
   let round = 1
-  while (scorePlyr1 < 3 && scorePlyr2 < 3) {
-    console.log('Round',round,':')
-    let p1choice = choose()
-    let p2choice = choose()
-    console.log('Choose :','P1',p1choice,'- P2',p2choice)
-    if (p1choice === p2choice) {
-      console.log('PAR !')
-    } else {
-      if (p1choice === 'rock') {
-        if (p2choice === 'paper') scorePlyr2++
-        else scorePlyr1++
-      } else if (p1choice === 'paper') {
-        if (p2choice === 'scissors') scorePlyr2++
-        else scorePlyr1++
-      } else if (p1choice === 'scissors') {
-        if (p2choice === 'rock') scorePlyr2++
-        else scorePlyr1++
-      }
-    }
-    console.log('Score :','P1',scorePlyr1,'- P2',scorePlyr2)
-    console.log('')
+  while (p1score < rounds || p2score < rounds) {
+    console.log('Round',round)
+    versus()
     round++
+    if (p1score === rounds || p2score === rounds) break;
   }
-
-  /*let round = 1
-  for (let i = 1; i <= round ; i++) {
-    console.log('Game',i,':')
-    let p1choice = choose()
-    let p2choice = choose()
-    console.log('Choose :','P1',p1choice,'- P2',p2choice)
-    if (p1choice === p2choice) {
-      round++
-      console.log('PAR !')
-    } else {
-      if (p1choice === 'rock') {
-        if (p2choice === 'paper') scorePlyr2++
-        else scorePlyr1++
-      } else if (p1choice === 'paper') {
-        if (p2choice === 'scissors') scorePlyr2++
-        else scorePlyr1++
-      } else if (p1choice === 'scissors') {
-        if (p2choice === 'rock') scorePlyr2++
-        else scorePlyr1++
-      }
-    }
-    console.log('Score :','P1',scorePlyr1,'- P2',scorePlyr2)
-    console.log('')
-    if (scorePlyr1 <= 3 || scorePlyr2 <= 3) round++
-  }*/
+  if (p1score === rounds) console.log('Player 1 win the game')
+  else console.log('Player 2 win the game')
 }
 
 game()
