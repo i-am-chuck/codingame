ebb62a8198aea0156dc52a616cac53fbcb3f4737
Antony <antony.caron.burdiak@gmail.com>
2022-11-24 01:07:08 +0100

    Add comments

--- ascii-art.js
+++ ascii-art.js
@@ -2,17 +2,24 @@ const L = parseInt(readline());
 const H = parseInt(readline());
 const T = readline();
 
+// define character list
 const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ?'
-let result = ''
 
+// loop on each line based on the height
 for (let i = 0; i < H; i++) {
   const ROW = readline();
   let line = '';
+  // decompose the word to print
   T.split('').forEach(letter => {
-    const validLetter = alpha.includes(letter.toUpperCase())
-    let alphaPos = alpha.length
-    if (validLetter) alphaPos = (letter.toLowerCase().charCodeAt() - 96)
-    const offset = (alphaPos * L) - L
+    // check if letter is present in list
+    const letterIsValid = alpha.includes(letter.toUpperCase())
+    // by default we use the last character (?)
+    let letterIndex = alpha.length
+    // if letter is valid we assign the index
+    if (letterIsValid) letterIndex = (letter.toLowerCase().charCodeAt() - 96)
+    // we define the offset for the start of the character
+    const offset = (letterIndex * L) - L
+    // we splice the line with the good signs to mach one line of the character
     const log = ROW.split('').slice(offset, offset+L).join('')
     line += log
   })
