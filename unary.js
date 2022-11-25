b5337352cf7d49c6e0483233f51724a275079c9f
Antony <antony.caron.burdiak@gmail.com>
2022-11-25 16:41:20 +0100

    Codingame puzzle 'unary'

--- unary.js
+++ unary.js
@@ -1,22 +1,30 @@
 /*const MESSAGE = readline();*/
 const MESSAGE = 'C';
-let array = []
+
 let result = ''
 
 MESSAGE.split('').forEach(letter => {
+
   let binary = letter.charCodeAt(0).toString(2)
-  console.log(binary)
   let binaryArr = binary.split('')
+
   let prevNumber = false
   let count = 1
-  binaryArr.forEach((number) => {
-    console.log(number)
+
+  binaryArr.forEach((number, index) => {
+
     if (prevNumber === number) count++
-    else array.push({number: number, count: count})
+    else count = 1
+
+    if (count === 1) {
+      if (index) result += ' '
+      result += number === '0' ? '0' : '00'
+      result += ' '
+    }
+    result += '0'
+
     prevNumber = number
   })
 })
-
-console.log(array)
 
 console.log(result);
