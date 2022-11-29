d908d0fcc9ff70824affb320be60f02891bedbaf
Antony <antony.caron.burdiak@gmail.com>
2022-11-29 10:55:39 +0100

    Codingame puzzle 'unary'

--- unary.js
+++ unary.js
@@ -1,30 +1,44 @@
-/*const MESSAGE = readline();*/
-const MESSAGE = 'C';
+/* https://www.codingame.com/ide/puzzle/unary */
 
-let result = ''
+/*const MESSAGE = readline();*/
+
+/* ✅ */
+/* const MESSAGE = 'C'; */
+/* result = 0 0 00 0000 0 00 */
+
+/* ✅ */
+/* const MESSAGE = 'CC'; */
+/* result = 0 0 00 0000 0 000 00 0000 0 00 */
+
+/* ❌ */
+ const MESSAGE = '%';
+/* result = 00 0 0 0 00 00 0 0 00 0 0 0 */
+
+let binaryArr = []
 
 MESSAGE.split('').forEach(letter => {
-
   let binary = letter.charCodeAt(0).toString(2)
-  let binaryArr = binary.split('')
+  binary.split('').forEach(number => { binaryArr.push(number) })
+})
 
-  let prevNumber = false
-  let count = 1
+let prevNumber = false
+let count = 1
+let result = ''
 
-  binaryArr.forEach((number, index) => {
+binaryArr.forEach((number, index) => {
 
-    if (prevNumber === number) count++
-    else count = 1
+  if (prevNumber === number) count++
+  else count = 1
 
-    if (count === 1) {
-      if (index) result += ' '
-      result += number === '0' ? '0' : '00'
-      result += ' '
-    }
-    result += '0'
+  if (count === 1) {
+    if (index) result += ' '
+    result += number === '0' ? '00' : '0'
+    result += ' '
+  }
+  result += '0'
 
-    prevNumber = number
-  })
+  prevNumber = number
 })
 
 console.log(result);
+console.log('00 0 0 0 00 00 0 0 00 0 0 0');
